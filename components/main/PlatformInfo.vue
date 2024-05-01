c9dc39d66ac6e10e40bf8dcf283bc37f4959bc3c
Oliver Whitehead <oliver@synx.games>
2024-05-01 18:46:26 +0100

    feat: split up homepage

---
+++ components/main/PlatformInfo.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="bg-white">
+    <div class="container w-full max-w-5xl mx-auto space-y-4">
+      <h3 class="text-3xl font-bold text-center text-[#005EB8] py-5">What is Doctors on hand?</h3>
+      <p class="text-lg text-center text-black">Introducing Doctors on Hand: a revolutionary service poised to redefine
+        the healthcare experience. Our mission is simple yet profound: to seamlessly connect individuals with the most
+        appropriate medical professionals, bridging the gap between patients and the NHS or their GPs.</p>
+      <p class="text-lg text-center text-black">With Doctors on Hand, your journey begins by inputting your symptoms,
+        initiating a personalised matching process. Through advanced algorithms, we identify the ideal medical expert
+        best suited to address your specific concerns.</p>
+    </div>
+
+  </section>
+</template>
+
+<style scoped>
+
+</style>
