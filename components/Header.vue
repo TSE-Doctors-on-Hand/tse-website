9dfb8e0c75c336fabb0b899e44767200872f0678
Oliver Whitehead <oliver@synx.games>
2024-05-01 18:46:12 +0100

    feat: fixed navbar and emergency banner

--- components/Header.vue
+++ components/Header.vue
@@ -1,74 +1,21 @@
 <template>
-    
-
-<nav class="bg-white border-gray-200 dark:bg-[#005EB8]">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-    <Icon name="medical-icon:i-cardiology" color="white" /> 
-  </a>
-  <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-      <button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-        <span class="sr-only">Open user menu</span>
-        
-
-
-      </button>
-      <a class="text-white max-sm:hidden font-medium pr-4 content-center items-center mx-auto justify-between ">My Account</a>
-        <Icon name="mdi:account" color="white" class="w-8 h-8 rounded-full" />
-      <!-- Dropdown menu -->
-      <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
-        <div class="px-4 py-3">
-          <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-          <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
-        </div>
-        <ul class="py-2" aria-labelledby="user-menu-button">
-          <li>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
-          </li>
-          <li>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
-          </li>
-          <li>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
-          </li>
-          <li>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
-          </li>
-        </ul>
-      </div>
-      <button data-collapse-toggle="navbar-user" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-  </div>
-  <div class="text-white items-center justify-center hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-    <span class="self-center text-2xl font-semibold ">Doctors On Hand</span>
-
-    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 rtl:space-x-reverse md:flex-row md:mt-0 justify-centre">
-       <!-- <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white ">Doctors On Hand</span>-->
-
-    <!--
-      <li>
-        <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+  <nav class="bg-white border-gray-200 dark:bg-[#005EB8]">
+    <ul class="max-w-screen-xl flex items-center justify-between mx-auto p-3">
+      <li class="w-48">
+        <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+          <Icon name="medical-icon:i-cardiology" color="white" />
+        </a>
       </li>
-      <li>
-        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+      <li class="flex-1 flex items-center justify-center">
+        <span class="text-2xl font-semibold text-white">Doctors On Hand</span>
       </li>
-      <li>
-        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+      <li class="w-48 flex items-center justify-end">
+        <a class="text-white max-sm:hidden font-medium pr-4 content-center items-center mx-auto justify-end">
+          <Icon name="mdi:account" color="white" class="w-8 h-8 rounded-full" /> My Account</a>
       </li>
-      <li>
-        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Pricing</a>
-      </li>
-      <li>
-        <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-      </li>
-    -->
     </ul>
-  </div>
-  </div>
-</nav>
-
+  </nav>
 </template>
+
+<script setup lang="ts">
+</script>
---
+++ components/main/EmergencyBanner.vue
@@ -0,0 +1,9 @@
+<template>
+
+  <div class="relative isolate flex items-center justify-center overflow-hidden bg-red-700 px-6 py-2.5 sm:px-3.5">
+    <p class="text-sm leading-6 text-white">
+      <strong class="font-semibold">In the event of an Emergency Call 999.</strong>
+    </p>
+  </div>
+
+</template>
