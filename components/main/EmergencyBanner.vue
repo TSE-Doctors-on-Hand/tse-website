baa9db57f7cdde87573060dbecc8ce4ce63f85cd
Harry-Collins <26336134@students.lincoln.ac.uk>
2024-05-02 14:42:53 +0100

    style: added icons

--- components/main/EmergencyBanner.vue
+++ components/main/EmergencyBanner.vue
@@ -1,8 +1,10 @@
 <template>
 
-  <div class="relative isolate flex items-center justify-center overflow-hidden bg-red-700 px-6 py-2.5 sm:px-3.5">
+  <div class="relative isolate flex items-center justify-center overflow-hidden bg-[#DA291C] px-6 py-2.5 sm:px-3.5">
     <p class="text-sm leading-6 text-white">
-      <strong class="font-semibold">In the event of an Emergency Call 999.</strong>
+      <Icon name="mdi:alert" color="white"/>
+      <strong class="font-semibold"> In the event of an Emergency Call 999 </strong>
+      <Icon name="mdi:alert" color="white"/>
     </p>
   </div>
 
