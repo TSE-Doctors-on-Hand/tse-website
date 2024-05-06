87de8122bc55381f65dab38eccd8476411dfb506
Oliver Whitehead <xfawks@gmail.com>
2024-05-06 18:13:41 +0100

    feat: rejig login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,56 +1,57 @@
 <template>
 
   <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
     </div>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <form class="space-y-6" @submit.prevent="handleLogin">
-        <div>
-          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
-          <div class="mt-2">
 
-            <input
-                v-model="username"
-                type="text"
-                placeholder="username"
-                class="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            >
+          <div class="mt-2 justify-content-center content-end">
 
-          </div>
-        </div>
-
-        <div>
-          <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-            <div class="text-sm">
-              <a href="#" class="font-semibold text-blue-600 hover:text-blue-500">Forgot password?</a>
+            <div class="card flex">
+              <FloatLabel>
+                <InputText id="username" v-model="username" />
+                <label for="username">Username</label>
+              </FloatLabel>
             </div>
-          </div>
-          <div class="mt-2">
 
-            <input
-                v-model="password"
-                type="password"
-                placeholder="password"
-                class="block w-full rounded-md px-2 border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            >
+            <div class="py-6"></div>
+
+            <div class="card flex justify-content-center">
+              <FloatLabel>
+                <Password id="password" toggleMask v-model="password" :feedback="false" />
+                <label for="password">Password</label>
+              </FloatLabel>
+            </div>
+
+            <div class="text-center py-3">
+              <div v-if="loggedInFail">
+                <p>Login Failed!</p>
+              </div>
+              <div v-if="!loggedInFail">
+                <p>Login Successful!</p>
+              </div>
+            </div>
+
 
           </div>
-        </div>
 
         <div>
-          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+          <button type="submit" class="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">Sign in</button>
         </div>
+
       </form>
 
       <p class="mt-10 text-center text-sm text-gray-500">
         Not a member?
-        <a href="#" class="font-semibold leading-6 text-blue-600 hover:text-blue-500">Sign Up</a>
+        <a href="/register" class="font-semibold leading-6 text-blue-600 hover:text-blue-500">Sign Up</a>
       </p>
     </div>
   </div>
+
 </template>
 <script lang="ts" setup>
 
@@ -62,8 +63,11 @@ import type { LoginRequest } from "~/types/api/auth/login";
 
 import { auth } from "~/composables/auth";
 
-const username = ref('username')
-const password = ref('password')
+const username = ref()
+const password = ref()
+
+const loggedInFail = ref()
+
 const router = useRouter()
 
 const handleLogin = () => {
@@ -72,7 +76,9 @@ const handleLogin = () => {
     password: password.value
   }
 
-  auth().login(userData)
+  auth().login(userData).then((bool) => {
+    loggedInFail.value = bool
+  })
 
 }
 </script>
