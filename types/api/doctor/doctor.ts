ddc8da2ea6f7497b476babb1bbd014f16e741dab
Oliver Whitehead <xfawks@gmail.com>
2024-05-08 21:56:27 +0100

    feat: implement MatchedDoctor

---
+++ types/api/doctor/doctor.ts
@@ -0,0 +1,14 @@
+export interface MatchedDoctor {
+
+    forename: string;
+    surname: string;
+    aboutMe: string;
+    specialties: string[];
+    practiceName: string;
+    practiceAddress: string;
+    email: string;
+    phone: string;
+    similarity: number;
+    distance: number;
+
+}
