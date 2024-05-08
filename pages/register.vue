2d95f701bded85cde5fbb7755e193194c7c45b45
Oliver Whitehead <xfawks@gmail.com>
2024-05-08 15:37:51 +0100

    feat: add register callback to login

--- pages/register.vue
+++ pages/register.vue
@@ -97,6 +97,11 @@
         <button type="submit" class="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">Sign Up</button>
       </div>
 
+      <p class="mt-10 text-center text-sm text-gray-500">
+        Already a member?
+        <a href="/login" class="font-semibold leading-6 text-blue-600 hover:text-blue-500">Sign In</a>
+      </p>
+
     </form>
 
   </div>
