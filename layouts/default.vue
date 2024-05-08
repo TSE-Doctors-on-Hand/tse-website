f2f6e1a189ac20c2543cb32e3612839f84e99704
Oliver Whitehead <xfawks@gmail.com>
2024-05-09 00:47:42 +0100

    comment: default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,8 @@
+<!--
+ Template for the entire Website
+ This allows us to define the Header, Footer and Emergency banner
+ across the entire site without the need to call it more than once.
+ -->
 <template>
   <div class="global-container font-spacegrotesk mx-auto bg-white text-black">
     <main>
