69bdf0398c1efc6396bc405b004c06ea1d65b235
Oliver Whitehead <xfawks@gmail.com>
2024-05-08 16:22:00 +0100

    feat: test doctors page for consult

---
+++ pages/service/doctors/[doctors].vue
@@ -0,0 +1,19 @@
+<template>
+
+  {{ route.params }}
+
+  {{ route.params.doctors }}
+
+</template>
+
+<script setup lang="ts">
+
+const route = useRoute()
+
+</script>
+
+<style scoped>
+
+
+
+</style>
